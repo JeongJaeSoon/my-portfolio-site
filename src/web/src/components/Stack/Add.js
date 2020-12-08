6913e7af5ece90b559e4d411e2445ca9d2456b58
JeongJaeSoon <wjdwotns1006@gmail.com>
2020-12-08 15:34:37 +0900

    [publishing] 기술스택 추가

--- src/web/src/components/Stack/Add.js
+++ src/web/src/components/Stack/Add.js
@@ -1,11 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 import "./Add.css";
+import "./ModalStack.css";
+
+Modal.setAppElement("#root");
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: 350,
+  },
+};
 
 const StackAdd = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const afterOpenModal = () => {
+    // subtitle.style.color = "#6B91FD";
+    // subtitle.style.fontSize = "15px";
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="stack-add">
       <div className="top">
-        <button className="add-btn">+</button>
+        <button onClick={openModal} className="add-btn">
+          +
+        </button>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Stack Add"
+        >
+          <div className="stack-modal">
+            <div className="header">
+              <div className="sub-title">Add Tech Stack </div>
+              <button onClick={closeModal} className="close-btn">
+                &#10005;
+              </button>
+            </div>
+            <div className="section">
+              <input type="text" className="name" placeholder="이름" />
+              <input
+                type="file"
+                name="stack_img"
+                className="img-btn"
+                onChange={null}
+              />
+              <ul>
+                <li>
+                  <p className="title">숙련도</p>
+                  <p className="value">
+                    <select name="stack-skillful">
+                      <option value="최상">최상</option>
+                      <option value="상">상</option>
+                      <option value="중상">중상</option>
+                      <option value="중">중</option>
+                    </select>
+                  </p>
+                </li>
+                <li>
+                  <p className="title">사용빈도</p>
+                  <p className="value">
+                    <input type="text" className="stack-frequency" />
+                  </p>
+                </li>
+                <li>
+                  <p className="title">대표색상</p>
+                  <p className="value">
+                    <input type="color" class="stack-color" value="#000000" />
+                  </p>
+                </li>
+              </ul>
+              <button className="save-btn">저장</button>
+            </div>
+          </div>
+        </Modal>
       </div>
       <div className="bottom">ADD</div>
     </div>
--- src/web/src/components/Stack/Home.js
+++ src/web/src/components/Stack/Home.js
@@ -3,9 +3,11 @@ import Title from "../Util/Title";
 import Item from "./Item";
 import Add from "./Add";
 import "./Home.css";
+
 const StackHome = () => {
   const data = [
     {
+      id: 5,
       name: "PHP",
       img_url:
         "https://blog.kakaocdn.net/dn/pVD59/btquknGhVh1/ArEncBG0uVuOvaF8TAdw31/img.jpg",
@@ -14,6 +16,7 @@ const StackHome = () => {
       frequency: 80,
     },
     {
+      id: 4,
       name: "JAVASCRIPT",
       img_url:
         "https://media.vlpt.us/images/ansrjsdn/post/f97332df-ff0a-45ae-91d9-f4825be458e6/js.png",
@@ -22,6 +25,7 @@ const StackHome = () => {
       frequency: 70,
     },
     {
+      id: 3,
       name: "REACT.JS",
       img_url:
         "https://media.vlpt.us/images/leejh9022/post/da20dd70-fd30-44ea-9f31-38aa8a56a3c6/reactjs-thumb.jpg",
@@ -35,7 +39,8 @@ const StackHome = () => {
       <Title titleName="Tech Stack" />
       <div className="stacks">
         {data.map((element) => {
-          return <Item stackData={element} />;
+          const { id } = element;
+          return <Item key={id} stackData={element} />;
         })}
         <Add />
       </div>
