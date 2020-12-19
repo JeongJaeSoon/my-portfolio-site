0827d63c6e6b28498bc091fded08e52d960b8fb0
JeongJaeSoon <wjdwotns1006@gmail.com>
2020-12-19 14:19:26 +0900

    [bug-fix] 프로젝트 상세내용 줄임말 수정

--- src/web/src/components/Project/View.js
+++ src/web/src/components/Project/View.js
@@ -119,7 +119,7 @@ const ProjectView = ({ projectId }) => {
           className="img"
           style={{
             background: `url(${img_url}) center no-repeat`,
-            backgroundSize: "contain",
+            backgroundSize: "200px",
           }}
         ></div>
         <ul className="info">
